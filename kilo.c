5158dccec10033873d5da268aa357a8db6c9c00b
YBS18 <batu.saatci18@gmail.com>
2019-01-29 22:30:38 +0300

    added raw mode terminal

--- kilo.c
+++ kilo.c
@@ -3,10 +3,15 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <errno.h>
 
+
+// defines
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 void enableRawMode();
 void disableRawMode();
+void die(const char *s);
 
 struct termios originalTermios;
 
@@ -14,18 +19,23 @@ int main()
 {
 	enableRawMode();
 
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+	while (1)
 	{
+		char c = '\0';
+		//read terminal for input
+		if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+		//check if control char
 		if (iscntrl(c))
 		{
-			printf("%d\n", c);
+			printf("%d\r\n", c);
 		}
 		else
 		{
-			printf("%d ('%c')\n",c ,c);
+			printf("%d ('%c')\r\n", c, c);
 		}
+		if (c == CTRL_KEY('q')) break;
 	}
+
 	return 0;
 }
 
@@ -33,18 +43,32 @@ int main()
 void enableRawMode()
 {
 	//read terminal attributes to strcut raw	
-	tcgetattr(STDIN_FILENO, &originalTermios);
+	if (tcgetattr(STDIN_FILENO, &originalTermios) == -1) die("tcgetattr");
 	atexit(disableRawMode);
 	
 	struct termios raw = originalTermios;
-	// turns off echo and switches off canonical mode
-	raw.c_lflag &= ~(ECHO | ICANON);
+	// turns off signals for raw mode
+	raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+	raw.c_cflag |= (CS8);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	//read timeout values
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
 	
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); 
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr"); 
 }
 
 //disables terminal Raw mode
 void disableRawMode()
 {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &originalTermios);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &originalTermios) == -1)
+		die("tcsetattr");
+}
+
+//error producing function
+void die(const char *s)
+{
+	perror(s);
+	exit(1);
 }
