158af4256eb07be9730108d5a431b7fdf9e8c2d5
YBS18 <batu.saatci18@gmail.com>
2019-01-30 20:20:51 +0300

    added append buffer

--- kilo.c
+++ kilo.c
@@ -4,10 +4,30 @@
 #include <termios.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/ioctl.h>
+#include <string.h>
 
 
 // defines
 #define CTRL_KEY(k) ((k) & 0x1f)
+#define ABUF_INIT {NULL, 0}
+#define KILO_VERSION "0.0.1"
+
+struct editorConfig
+{
+	struct termios originalTermios;
+	int screenRows;
+	int screenCols;	
+};
+
+struct editorConfig E; 
+
+struct abuf {
+	char *b;
+	int len;
+};
+
+
 
 void enableRawMode();
 void disableRawMode();
@@ -15,20 +35,22 @@ void die(const char *s);
 char editorReadKey();
 void editorProcessKeypress();
 void editorRefreshScreen();
-void editorDrawRows();
+void editorDrawRows(struct abuf *ab);
+int getWindowSize(int *rows, int *cols);
+void initEditor();
+int getCursorPosition(int *rows, int *cols);
+void abAppend(struct abuf *ab, const char *s, int len);
+void abFree(struct abuf *ab);
 
 
-struct editorConfig
-{
-	struct termios originalTermios;	
-};
 
-struct editorConfig E; 
+
 
 
 int main()
 {
 	enableRawMode();
+	initEditor();
 
 	while (1)
 	{
@@ -106,21 +128,119 @@ void editorProcessKeypress()
 	}
 }
 
+//clears screen
 void editorRefreshScreen()
 {
-	write(STDOUT_FILENO, "\x1b[2J", 4);
-	write(STDOUT_FILENO, "\x1b[H", 3);
+	struct abuf ab = ABUF_INIT;
 
-	editorDrawRows();
+	abAppend(&ab, "\x1b[?25l", 6);
+	abAppend(&ab, "\x1b[H", 3);
 
-	write(STDOUT_FILENO, "\x1b[H", 3);
+	editorDrawRows(&ab);
+
+	abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[?25h", 6);
+
+	write(STDOUT_FILENO, ab.b, ab.len);
+	abFree(&ab);
 }
 
-void editorDrawRows()
+//draws tildas to terminal based on size
+void editorDrawRows(struct abuf *ab)
 {
 	int y;
-	for (y = 0; y < 24; y++)
+	for (y = 0; y < E.screenRows; y++)
 	{
-		write(STDOUT_FILENO, "~\r\n", 3);
+		if (y == E.screenRows / 3)
+		{
+			char welcome[80];
+			int welcomelen = snprintf(welcome, sizeof(welcome), 
+				"Kilo editor -- version %s", KILO_VERSION);
+			if (welcomelen > E.screenCols) welcomelen = E.screenCols;
+			int padding = (E.screenCols - welcomelen) / 2;
+			if (padding)
+			{
+				abAppend(ab, "~", 1);
+				padding--;
+			}
+			while (padding--) abAppend(ab, " ", 1);
+			abAppend(ab, welcome, welcomelen);
+		}
+		else
+		{
+			abAppend(ab, "~", 1);
+		}
+
+		abAppend(ab, "\x1b[K", 3);
+		if (y < E.screenRows - 1)
+		{
+			abAppend(ab, "\r\n", 2);
+		}
 	}
+}
+
+//gets the terminal size
+int getWindowSize(int *rows, int *cols)
+{
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+	{
+		//sends cursor right down if ioctl cant get the size of terminal
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+		{
+			return -1;
+		}
+
+		return getCursorPosition(rows, cols);
+	}
+	else
+	{
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
+// initializes struct editor
+void initEditor()
+{
+	if (getWindowSize(&E.screenRows, &E.screenCols) == -1) die("getWindowSize");
+}
+
+int getCursorPosition(int *rows, int *cols)
+{
+	char buf[32];
+	unsigned int i = 0;
+
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+		return -1;
+
+	while (i < sizeof(buf) - 1)
+	{
+		if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+		if (buf[i] == 'R') break;
+		i++;
+	}
+	buf[i] = '\0';
+
+	if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+	return 0;
+}
+
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+	char *new = realloc(ab->b, ab->len + len);
+
+	if (new == NULL) return;
+	memcpy(&new[ab->len], s, len);
+	ab->b = new;
+	ab->len += len;
+}
+
+void abFree(struct abuf *ab)
+{
+	free(ab->b);
 }
